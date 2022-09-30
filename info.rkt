79794c6b81a86a03c67a80bb9bc5054af34d5b7c
Lukas Lazarek <LukasALazarek@gmail.com>
2022-09-30 14:31:18 -0500

    Add info.rkt

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "pict-util")
+(define deps '("base"
+               "slideshow"
+               "pict-lib"))
+(define build-deps '("at-exp-lib"))
+(define pkg-desc "Pict utilities")
+(define pkg-authors '(lukas))
+
